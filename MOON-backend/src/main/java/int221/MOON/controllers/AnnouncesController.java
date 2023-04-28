ed55a9c7ac9245bc9e039e76b43f0f2d6d2bf6f1
polpolKung <chanpol.thip@kmutt.ac.th>
2023-04-28 14:02:10 +0700

    แก้ back @Crossorigin แก้ app propeties

--- MOON-backend/src/main/java/int221/MOON/controllers/AnnouncesController.java
+++ MOON-backend/src/main/java/int221/MOON/controllers/AnnouncesController.java
@@ -5,13 +5,10 @@ import int221.MOON.Dto.AnnouncesDto;
 import int221.MOON.entities.Announces;
 import int221.MOON.service.AnnouncesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/announcements")
 public class AnnouncesController {
