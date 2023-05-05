16b4c1db92296dc5ae24534d46041ff43752125b
Anon1166 <anon.2222@mail.kmutt.ac.th>
2023-05-05 17:34:40 +0700

    edit ui

--- MOON-front/src/components/AnnouncementForm.vue
+++ MOON-front/src/components/AnnouncementForm.vue
@@ -194,8 +194,8 @@ const submit = async () => {
                                     to
                                     show this announcement</label>
                             </div>
-                            <button type="submit" :disabled="check()"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                            <button type="submit" :disabled="check()" :class="check() ? '' : 'hover:bg-blue-800 dark:hover:bg-blue-700'"
+                                class="text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600  dark:focus:ring-blue-800  disabled:bg-zinc-600 disabled:text-zinc-400 ">Submit</button>
                         </form>
                         <button @click="$router.go(-1)"
                             class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Cancel</button>
