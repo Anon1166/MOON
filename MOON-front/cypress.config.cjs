ab31ca5f4ebf15310acd62ef24849d888ebae681
polpolKung <chanpol.thip@kmutt.ac.th>
2023-05-15 17:39:17 +0700

    cypress srpint3

--- MOON-front/cypress.config.cjs
+++ MOON-front/cypress.config.cjs
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-   baseUrl:"http://intproj22.sit.kmutt.ac.th/at2",
+   baseUrl:"http://intproj22.sit.kmutt.ac.th/at2s",
    experimentalSessionAndOrigin: true
   },
 });
